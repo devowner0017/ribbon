2505836d1d9fc7046cbde7c8237246bb17fdb3bb
devowner0017 <rascalhead1004@gmail.com>
2023-05-14 00:18:29 +0300

    update-2

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import { Question_One, Question_Two, Question_Three } from '../views/question'
-
-import Home from '../views/Home.vue';
+import { Question_One, Question_Two, Question_Three, Question_Four } from '../views/question'
 import Upbeat from '../views/question/Upbeat.vue';
+import Home from '../views/Home.vue';
+import Four_continue from '../views/question/Four_continue.vue';
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   mode: 'history',
@@ -29,10 +29,20 @@ const router = createRouter({
       component: Question_Three
     },
     {
+      path: '/questions/4',
+      name: 'question4',
+      component: Question_Four
+    },
+    {
       path: '/questions/3/upbeat',
       name: 'upbeat',
-      component: Upbeat
+      component: Upbeat,
     },
+    {
+      path: '/questions/4/continue',
+      name:'continue',
+      component: Four_continue,
+    }
   ]
 })
 
--- src/views/question/Four.vue
+++ src/views/question/Four.vue
@@ -1,3 +1,75 @@
 <template>
-    this is question four page.
+    <div class="flex flex-col grow">
+        <MenuBar />
+        <div class="w-full flex grow grid md:grid-cols-2 sm:gird-cols-1 ">
+            <div class="md:flex flex-col grow w-full justify-center items-center hidden">
+                <div class="form-content form-content-md form-content-sd form-content-sm">
+                    <div class="text-main-content mb-4 w-full">How is this? Would you like it more:
+                    </div>
+                    <div class=" grid grid-cols-2">
+                        <ToggleCard text="holds a job" />
+                        <ToggleCard text="caretaker" />
+                        <ToggleCard text="recent illness" />
+                        <ToggleCard text="recent travel" />
+                        <ToggleCard text="Took time off" />
+                        <ToggleCard text="none apply" />
+                    </div>
+                    <div class="flex flex-row w-full">
+                        <Button class="btn-primary  mt-1 w-full" :onClick="onNextPage">continue</Button>
+                    </div>
+                </div>
+            </div>
+            <div class="primary-panel primary-panel-sd primary-panel-sm">
+                <Paper />
+            </div>
+        </div>
+        <div class=" md:hidden sm:block w-full fixed bottom-0 ">
+            <div class="flex justify-end">
+                <ShareButton class="share-btn-sm mb-6 mr-6">
+                    <div class="flex">
+                        <img src="/images/messageBox.svg" class="mr-2" alt="share" /> SHARE
+                    </div>
+                </ShareButton>
+            </div>
+
+            <div class="sm-tool-bar ">
+                <div class="text-main-content mb-4">How is this? Would you like it more:
+                </div>
+                <div class="grid w-full grid-cols-3">
+                    <ToggleCard text="holds a job" />
+                    <ToggleCard text="caretaker" />
+                    <ToggleCard text="recent illness" />
+                    <ToggleCard text="recent travel" />
+                    <ToggleCard text="Took time off" />
+                    <ToggleCard text="none apply" />
+                </div>
+                <div class="flex flex-row w-full">
+                    <Button class="btn-primary  mt-1 w-full" :onClick="onNextPage">continue</Button>
+                </div>
+            </div>
+        </div>
+
+    </div>
 </template>
+<script>
+import MenuBar from '../../components/MenuBar.vue';
+import ToggleCard from '../../components/ToggleCard.vue';
+import Button from '../../components/Button.vue';
+import Paper from '../../components/Paper.vue';
+import ShareButton from '../../components/ShareButton.vue';
+export default {
+    components: {
+        ToggleCard,
+        MenuBar,
+        Button,
+        Paper,
+        ShareButton
+    },
+    name: 'Second Question',
+    methods: {
+        onNextPage() {
+            this.$router.push('/questions/4/continue')
+        }
+    }
+}
+</script>
