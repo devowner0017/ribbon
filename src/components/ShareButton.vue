7203510b4686abd652e8c430ca629c90cac62a6e
devowner0017 <rascalhead1004@gmail.com>
2023-05-13 13:07:15 +0300

    update

--- src/components/ShareButton.vue
+++ src/components/ShareButton.vue
@@ -1,23 +1,33 @@
 <template>
-    <button :class="Class">
+    <button :class="Class" @click="onClickShare">
         <slot></slot>
     </button>
 </template>
 <script>
+
 export default {
-    name: 'ShareButton',
+    name: "ShareButton",
     props: {
         class: {
             type: String,
             default: "",
         }
     },
+    data() {
+        return {
+            isModalVisible: false,
+        };
+    },
+    methods: {
+        onClickShare() {
+            this.isModalVisible = true;
+        }
+    },
     computed: {
         Class() {
             return "share-btn " + this.class;
         }
-    }
-
+    },
 }
 </script>
 <style scoped lang="scss">
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import { Question_One, Question_Two, Question_Three, Question_Four } from '../views/question'
+import { Question_One, Question_Two, Question_Three } from '../views/question'
+
 import Home from '../views/Home.vue';
+import Upbeat from '../views/question/Upbeat.vue';
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   mode: 'history',
@@ -27,10 +29,10 @@ const router = createRouter({
       component: Question_Three
     },
     {
-      path: '/questions/4',
-      name: 'question4',
-      component: Question_Four
-    }
+      path: '/questions/3/upbeat',
+      name: 'upbeat',
+      component: Upbeat
+    },
   ]
 })
 
