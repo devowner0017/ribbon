877a99ecebe05b96013c429bfe988457859660e3
devowner0017 <rascalhead1004@gmail.com>
2023-05-19 19:05:14 +0300

    change backend url

--- src/actions/generate.js
+++ src/actions/generate.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default function generateAnswer(question) {
     return new Promise((resolve, reject) => {
-         axios.get(`http://localhost:5000/api/chatgpt?question=+${question}`).then( res => {
+         axios.get(`http://eg-backend.vercel.app/api/chatgpt?question=+${question}`).then( res => {
             resolve(res.data)
         }).catch((error) => {
             console.log(error.data);
