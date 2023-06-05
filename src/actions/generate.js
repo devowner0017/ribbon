6486f59f48bb1fb7196fc414dbf7ee5567ceb95b
devowner0017 <rascalhead1004@gmail.com>
2023-06-06 00:57:45 +0300

    change base url

--- src/actions/generate.js
+++ src/actions/generate.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default function generateAnswer(question) {
     return new Promise((resolve, reject) => {
-         axios.post(`https://seashell-app-yyfz7.ondigitalocean.app/api/chatgpt`, {"question": question}).then( res => {
+         axios.post(`https://ribbon-app-gfoo7.ondigitalocean.app/api/chatgpt`, {"question": question}).then( res => {
             resolve(res.data)
         }).catch((error) => {
             console.log(error.data);
--- src/actions/hubspot.js
+++ src/actions/hubspot.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const sendDataToHubspot = (data) => {
     return new Promise((resolve, reject) => {
-        axios.post("https://seashell-app-yyfz7.ondigitalocean.app/api/hubspot", data)
+        axios.post("https://ribbon-app-gfoo7.ondigitalocean.app/api/hubspot", data)
             .then(res => resolve(res.data))
             .catch(err => reject(err));
     })
